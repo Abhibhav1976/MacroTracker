86e083682de7ec3a1cff1f649b27759029d1a0fc
Abhibhav <abhibhavr@gmail.com>
2024-12-23 16:46:09 +0530

    Initial Commit

---
+++ CalorieCalculator/CalorieCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieCalculatorApp.swift
+//  CalorieCalculator
+//
+//  Created by Abhibhav RajSingh on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CalorieCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
